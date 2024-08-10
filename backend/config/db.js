097f2044eebbc9875392020cf5b1fca7a12b848c
riplblr32 <itsupport1@rajamane.com>
2024-08-10 18:07:49 +0530

    new check

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // backend/config/db.js
 const sql = require('mssql');
 
@@ -24,30 +23,3 @@ const poolPromise = new sql.ConnectionPool(config)
 module.exports = {
     sql, poolPromise
 };
-=======
-// backend/config/db.js
-const sql = require('mssql');
-
-const config = {
-    user: 'your_username',
-    password: 'your_password',
-    server: 'your_server',
-    database: 'task_manager_db',
-    options: {
-        encrypt: true,
-        enableArithAbort: true,
-    },
-};
-
-const poolPromise = new sql.ConnectionPool(config)
-    .connect()
-    .then(pool => {
-        console.log('Connected to MSSQL');
-        return pool;
-    })
-    .catch(err => console.error('Database Connection Failed:', err));
-
-module.exports = {
-    sql, poolPromise
-};
->>>>>>> 7129b46e0e4079494352b703a9bd84047939ba64
